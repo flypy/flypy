afe99a2953b9c588572a431c4bb25a68a476e44e
Mark Florisson <markflorisson88@gmail.com>
2013-09-17 20:56:24 +0100

    Add Jon's TypedProperty from numba

--- numba2/utils.py
+++ numba2/utils.py
@@ -42,3 +42,31 @@ def applyable_decorator(decorator):
             return lambda f: decorator(f, *args, **kwargs)
 
     return decorator_wrapper
+
+
+class TypedProperty(object):
+    '''Defines a class property that does a type check in the setter.'''
+
+    def __new__(cls, ty, doc, default=None):
+        rv = super(TypedProperty, cls).__new__(cls)
+        cls.__init__(rv, ty, doc, default)
+        return property(rv.getter, rv.setter, rv.deleter, doc)
+
+    def __init__(self, ty, doc, default=None):
+        self.propname = '_numba_property_%d' % (id(self),)
+        self.default = default
+        self.ty = ty
+        self.doc = doc
+
+    def getter(self, obj):
+        return getattr(obj, self.propname, self.default)
+
+    def setter(self, obj, new_val):
+        if not isinstance(new_val, self.ty):
+            raise ValueError(
+                'Invalid property setting, expected instance of type(s) %r '
+                '(got %r).' % (self.ty, type(new_val)))
+        setattr(obj, self.propname, new_val)
+
+    def deleter(self, obj):
+        delattr(obj, self.propname)
