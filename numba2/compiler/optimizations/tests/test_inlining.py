5a72106e95103d01e0a65094f5e0f64197ef3d62
Mark Florisson <markflorisson88@gmail.com>
2013-10-16 12:16:02 +0100

    Test inlining

---
+++ numba2/compiler/optimizations/tests/test_inlining.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import
+
+import unittest
+
+from numba2 import jit, ijit
+
+#===------------------------------------------------------------------===
+# Tests
+#===------------------------------------------------------------------===
+
+class TestInlining(unittest.TestCase):
+
+    def test_inline_simple(self):
+        @ijit
+        def g(x):
+            return x * 2
+        @ijit
+        def f(x):
+            return g(x) + 2
+
+        self.assertEqual(f(8), 18)
+
+if __name__ == '__main__':
+    unittest.main()
