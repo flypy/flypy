f3ed507134b5c848f405302396b62ab14497a1e5
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-11-11 13:25:25 -0600

    Fixes for boehmlib.pyx

--- numba2/runtime/gc/boehmlib.pyx
+++ numba2/runtime/gc/boehmlib.pyx
@@ -13,11 +13,11 @@ cdef extern from "gc.h":
     void GC_disable()
     void GC_enable()
 
-    ctypedef void (*GC_finalization_proc) (void * obj, void * client_data);
+    ctypedef void (*GC_finalization_proc) (void * obj, void * client_data)
 
     void GC_register_finalizer(void * obj, GC_finalization_proc fn,
                   void * cd, GC_finalization_proc *ofn,
-                  void * *ocd);
+                  void * *ocd)
 
 
 GC_INIT()
@@ -26,16 +26,16 @@ cdef public void boehm_collect():
     GC_gcollect()
 
 cdef public void *boehm_malloc(size_t nbytes):
-    return GC_MALLOC(nbytes)
+    GC_MALLOC(nbytes)
 
 cdef public void boehm_disable():
-    return GC_disable()
+    GC_disable()
 
 cdef public void boehm_enable():
-    return GC_enable()
+    GC_enable()
 
 cdef public void boehm_register_finalizer(void *obj, void *dtor):
     cdef GC_finalization_proc old_finalizer
     cdef void *old_client_data
 
-    GC_register_finalizer(obj, dtor, NULL, &old_finalizer, &old_client_data)
+    GC_register_finalizer(obj, <GC_finalization_proc>dtor, NULL, &old_finalizer, &old_client_data)
