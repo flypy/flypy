09912696916097c47e40f60310a485f003b0d613
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-11-20 12:03:44 -0600

    Improve test_len_range

--- numba2/runtime/tests/test_builtins.py
+++ numba2/runtime/tests/test_builtins.py
@@ -16,15 +16,25 @@ class TestBuiltins(unittest.TestCase):
         self.assertTrue(trivial())
 
     def test_len_range(self):
-        for start in range(-10, 10):
-            for stop in range(-11, 11):
-                for step in range(-12, 12):
-                    if step == 0:
-                        continue
+        # Original loop takes too long and it is unncessarily detailed.
+        # Should only necessary to check the boundaries and a few random
+        # number in between.
+        #
+        #for start in range(-10, 10):
+        #    for stop in range(-11, 11):
+        #        for step in range(-12, 12):
+        #            if step == 0:
+        #                continue
+        start_range = [-10, -1, 0, 1, 10]
+        stop_range = [-11, -1, 0, 1, 11]
+        step_range = [-12, -1, 1, 12]
+        for start in start_range:
+            for stop in stop_range:
+                for step in step_range:
                     self.assertEqual(len_range(start, stop, step),
                                      len(range(start, stop, step)))
 
 
 if __name__ == '__main__':
-    TestBuiltins('test_isinstance').debug()
-    #unittest.main()
+    #TestBuiltins('test_isinstance').debug()
+    unittest.main()
