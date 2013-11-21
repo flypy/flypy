ff6ad50eab4b4583501ddad2851d2a7f2c2bf9f8
Mark Florisson <markflorisson88@gmail.com>
2013-11-21 10:23:27 +0000

    Fix 'and' test (thanks @gdementen)

--- numba2/tests/test_basic.py
+++ numba2/tests/test_basic.py
@@ -50,7 +50,7 @@ class TestTranslation(unittest.TestCase):
         self.assertEqual(f(2, 12), 4)
         self.assertEqual(f(2, 3), 5)
         self.assertEqual(f(3, 2), 5)
-        self.assertEqual(f(12, 2), 5)
+        self.assertEqual(f(20, 12), 5)
 
     def test_or(self):
         @jit
