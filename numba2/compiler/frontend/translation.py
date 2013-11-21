c08f0ec681ad2b5d6d61bd7bd46e1e3905a58445
Mark Florisson <markflorisson88@gmail.com>
2013-11-21 17:27:44 +0000

    Use variable names in IR

--- numba2/compiler/frontend/translation.py
+++ numba2/compiler/frontend/translation.py
@@ -114,7 +114,8 @@ class Translate(object):
         # Setup Variables
         self.builder.position_at_beginning(self.dst.startblock)
         for varname in self.varnames:
-            stackvar = self.builder.alloca(types.Pointer(types.Opaque))
+            stackvar = self.builder.alloca(types.Pointer(types.Opaque),
+                                           result=varname)
             self.allocas[varname] = stackvar
 
             # Initialize function arguments
