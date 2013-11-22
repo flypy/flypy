16bbeb51e2ce00bb97c787e4120cde7447dc3a5d
Siu Kwan Lam <michael.lam.sk@gmail.com>
2013-11-22 14:57:33 -0600

    Fancier error message

--- numba2/errors.py
+++ numba2/errors.py
@@ -30,31 +30,57 @@ class SpecializeError(CompileError):
     Raised when we fail to specialize on something requested for some reason.
     """
 
+class _ErrorMsg(object):
+    def __init__(self, exc, lineno=-1, during=None, pyfunc=None):
+        self.exc = exc
+        self.lineno = lineno
+        self.during = during
+        self.pyfunc = pyfunc
+
+    def __str__(self):
+        try:
+            return self.format()
+        except Exception, e:
+            return str(e)
+
+    def format(self, level=0):
+        if self.pyfunc is not None:
+            func = 'in function %s ' % _tell_func(self.pyfunc)
+        else:
+            func = ''
+
+        if self.lineno >= 0:
+            lineno = 'at line %d ' % self.lineno
+        else:
+            lineno = ''
+
+        during = '%s ' % self.during if self.during else ''
+
+        try:
+            innerexc = self.exc.args[0]
+        except IndexError:
+            innerexc = self.exc
+        if isinstance(innerexc, _ErrorMsg):
+            inner = innerexc.format(level=level + 1)
+        else:
+            inner = 'Reason: %s' % innerexc
+
+        if level == 0:
+            leadline = '\n'
+        else:
+            leadline = '-' * level
+
+        return "%s%s%s%s\n%s" % (leadline, during, func, lineno, inner)
+
+
 @contextmanager
 def error_context(lineno=-1, during=None, pyfunc=None):
     # Adapted from numbapro/npm/errors.py
     try:
         yield
     except Exception, e:
-        msg = []
-        if pyfunc is not None:
-            during = during or ''
-            during += ' - %s' % _tell_func(pyfunc)
-
-        if lineno >= 0:
-            msg.append('At line %d:' % lineno)
-        if during:
-            msg.append('During: %s' % during)
-
-        if isinstance(e, AssertionError):
-            msg.append('Internal error: %s' % e)
-        elif isinstance(e, error):
-            msg.append(str(e))
-        else:
-            msg.append('%s: %s' % (type(e).__name__, e))
-
-        exctype = type(e)
-        exc = exctype('\n'.join(msg))
+        em = _ErrorMsg(exc=e, pyfunc=pyfunc, during=during, lineno=lineno)
+        exc = type(e)(em)
         raise exc, None, sys.exc_info()[2]
 
 
