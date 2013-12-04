7c6d38c95768a0712945305257bae191b6e0c3f9
Mark Florisson <markflorisson88@gmail.com>
2013-12-04 15:17:13 +0000

    Test broadcasting function

--- numba2/runtime/operators/vectorize.py
+++ numba2/runtime/operators/vectorize.py
@@ -5,10 +5,9 @@ numba.vectorize().
 """
 
 from __future__ import print_function, division, absolute_import
-from itertools import starmap
 
-from numba2 import sjit, jit, typeof, conversion, parse
-from numba2.runtime.obj.arrayobject import Dimension, EmptyDim
+from numba2 import sjit, jit, typeof, parse
+from numba2.runtime.obj.arrayobject import Array, Dimension, EmptyDim
 
 def vectorize(py_func, signatures, **kwds):
     signature = parse(signatures[0])
@@ -26,41 +25,53 @@ def make_ndmap(py_func, nargs):
 # broadcasting
 #===------------------------------------------------------------------===
 
-@jit('Array[dtype1, dims] -> Array[dtype2, dims]')
+@jit('Array[dtype1, dims1] -> Array[dtype2, dims2] -> r')
 def broadcast(a, b):
     """Broadcast two arrays"""
-    return _broadcast(a.dims, b.dims, a.dims, b.dims)
+    return _broadcast(a.dims, b.dims, a, b)
 
 @jit('Dimension[base] -> Dimension[base] -> a -> b -> r')
-def _broadcast(a, b, adims, bdims):
+def _broadcast(a, b, array1, array2):
     """Broadcast two given dimensions"""
     # Equivalent dimensions, done
-    return (a, b)
+    return (array1, array2)
 
-@jit('Dimension[base1] -> Dimension[base1] -> a -> b -> r')
-def _broadcast(a, b, adims, bdims):
+@jit('Dimension[base1] -> Dimension[base2] -> a -> b -> r')
+def _broadcast(a, b, array1, array2):
     # Reduce structure
-    return _broadcast(a.base, b.base, adims, bdims)
+    return _broadcast(a.base, b.base, array1, array2)
 
 @jit('Dimension[base] -> EmptyDim[] -> a -> b -> r')
-def _broadcast(a, b, adims, bdims):
+def _broadcast(a, b, array1, array2):
     # LHS has more dims, patch RHS with extra dimensions
-    return (adims, _patch(bdims, a))
+    dims2 = raise_level(array2.dims, a)
+    return (array1, Array(array2.data, dims2))
 
 @jit('EmptyDim[] -> Dimension[base] -> a -> b ->r')
-def _broadcast(a, b, adims, bdims):
+def _broadcast(a, b, array1, array2):
     # RHS has more dims, patch LHS with extra dimensions
-    return (_patch(adims, b), b)
+    dims1 = raise_level(array1.dims, b)
+    return (Array(array1.data, dims1), array2)
 
+# -- broadcast helper -- #
 
 @jit('Dimension[base1] -> Dimension[base2] -> Dimension[base3]')
-def _patch(dims, missing):
+def raise_level(dims, missing):
     """
-    Patch `dims` with broadcasting leading dimensions dictated by `missing`
+    Raise the level of `dims` by prepending broadcasting dimensions as
+    dictated by `missing`.
     """
-    base = _patch(dims, missing.base)
+    base = raise_level(dims, missing.base)
     return Dimension(base, 1, 0)
 
 @jit('Dimension[base1] -> EmptyDim[] -> Dimension[base3]')
-def _patch(dims, missing):
+def raise_level(dims, missing):
     return dims
+
+#===------------------------------------------------------------------===
+# test
+#===------------------------------------------------------------------===
+
+@jit('Array[dtype1, dims1] -> Array[dtype2, dims2] -> r')
+def add(a, b):
+    return broadcast(a, b)
