c3fd4d0dcad48db363a741457cc0271f3a9226ff
Mark Florisson <markflorisson88@gmail.com>
2013-12-05 16:23:43 +0000

    Some more numpy

--- numba2/lib/nplib.py
+++ numba2/lib/nplib.py
@@ -28,6 +28,18 @@ def empty_like(array, dtype=None):
     shape = array.getshape()
     return empty(shape, dtype)
 
+@jit
+def zeros_like(array, dtype=None):
+    result = empty_like(array, dtype)
+    result[:] = 0
+    return result
+
+@jit
+def ones_like(array, dtype=None):
+    result = empty_like(array, dtype)
+    result[:] = 1
+    return result
+
 @jit('shape -> Type[dtype] -> Array[dtype, dims]')
 def empty(shape, dtype):
     items = product(shape)
@@ -36,25 +48,38 @@ def empty(shape, dtype):
     dims = c_layout_from_shape(shape, dtype)
     return Array(data, dims, dtype)
 
+@jit('shape -> Type[dtype] -> Array[dtype, dims]')
+def zeros(shape, dtype):
+    result = empty(shape, dtype)
+    result[:] = 0
+    return result
+
+@jit('shape -> Type[dtype] -> Array[dtype, dims]')
+def ones(shape, dtype):
+    result = empty(shape, dtype)
+    result[:] = 0
+    return result
+
 #===------------------------------------------------------------------===
 # Helpers
 #===------------------------------------------------------------------===
 
 @jit('StaticTuple[a, b] -> dtype -> Dimension[base]')
 def c_layout_from_shape(shape, dtype):
+    """Construct dimensions for an array of shape `shape` with a C layout"""
     dim = c_layout_from_shape(tail(shape), dtype)
     extent = head(shape)
-    stride = dim.stride * extent
+    stride = dim.stride * dim.extent
     return Dimension(dim, extent, stride)
 
 @jit('StaticTuple[a, EmptyTuple[]] -> dtype -> Dimension[base]')
 def c_layout_from_shape(shape, dtype):
     extent = head(shape)
-    stride = sizeof(dtype)
-    return Dimension(EmptyDim(), extent, stride) # TODO: ContigDim
+    return Dimension(EmptyDim(), extent, 1) # TODO: ContigDim
 
 @jit
 def product(it):
+    """Take the product of the iterable"""
     prod = 1
     for x in it:
         prod *= x
@@ -66,3 +91,5 @@ def product(it):
 
 overlay(np.empty, empty)
 overlay(np.empty_like, empty_like)
+overlay(np.zeros_like, zeros_like)
+overlay(np.ones_like,  ones_like)
--- numba2/lib/tests/test_nplib.py
+++ numba2/lib/tests/test_nplib.py
@@ -9,7 +9,7 @@ import numpy as np
 
 class TestNumpyLib(unittest.TestCase):
 
-    def test_np_empty_like(self):
+    def test_np_empty(self):
         @jit
         def empty(shape, dtype):
             return np.empty(shape, dtype)
@@ -21,6 +21,43 @@ class TestNumpyLib(unittest.TestCase):
             self.assertEqual(a.strides, b.strides)
 
         test((5, 8), float64, np.float64)
+        test((8, 5), float64, np.float64)
+        test((5, 8), int32, np.int32)
+        test((8, 5), int32, np.int32)
+
+    def test_np_empty_like(self):
+        @jit
+        def empty_like(a, dtype):
+            return np.empty_like(a, dtype)
+
+        a = np.empty((10, 12), dtype=np.int64)
+
+        self.assertEqual(empty_like(a, int64).shape, a.shape)
+        self.assertEqual(empty_like(a, int64).strides, a.strides)
+        self.assertEqual(empty_like(a, float32).shape, a.shape)
+
+    def test_np_zeros_like(self):
+        @jit
+        def zeros_like(a, dtype):
+            return np.zeros_like(a, dtype)
+
+        a = np.zeros((10, 12), dtype=np.int64)
+
+        self.assertEqual(zeros_like(a, int64).shape, a.shape)
+        self.assertEqual(zeros_like(a, int64).strides, a.strides)
+        self.assertEqual(zeros_like(a, float32).shape, a.shape)
+        self.assertTrue(np.all(zeros_like(a, float32) == 0))
+
+    def test_np_empty_like_no_dtype(self):
+        @jit
+        def empty_like(a):
+            return np.empty_like(a)
+
+        a = np.empty((10, 12), dtype=np.int64)
+
+        # Test without dtype
+        self.assertEqual(empty_like(a).shape, a.shape)
+        self.assertEqual(empty_like(a).dtype, a.dtype)
 
 
 if __name__ == '__main__':
