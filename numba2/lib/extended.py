40359e783a9cf352152ae71f0995581508657602
Mark Florisson <markflorisson88@gmail.com>
2013-12-29 23:19:32 +0000

    Basic start on small stack-allocated arrays

--- numba2/lib/extended.py
+++ numba2/lib/extended.py
@@ -6,6 +6,8 @@ from __future__ import print_function, division, absolute_import
 
 from .pyobject import Object
 from .arrays.arrayobject import NDArray, Dimension, EmptyDim
+from .vectorobject import Vector
+
 try:
     from .decimalobject import decimal
 except ImportError:
